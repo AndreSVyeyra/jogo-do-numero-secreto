0c0e46c5eb4265d92cd4d8cb8394fd01bff7ce5b
Andre <andrevieira19992018@gmail.com>
2025-11-20 20:48:13 -0300

    criando função gerar número aleatorio

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+
 function exibirTextoNaTela(tag,texto) {
     let conteudo = document.querySelector(tag);
     conteudo.innerHTML = texto; 
 }
 
  function verificarChute() {
-    console.log("cliquei no botao")
+    console.log(numeroSecreto);
  };
-
 
 
 exibirTextoNaTela("h1","Jogo do número secreto");
 exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 
 
+function gerarNumeroAleatorio(){
+    const numeroAleatorio = parseInt(Math.random() * 10 + 1);
+    return numeroAleatorio;
+}
+
+let numeroSecreto = gerarNumeroAleatorio();
