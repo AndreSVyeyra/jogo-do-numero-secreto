e1b738f3622d2195a3d77d601111ddeeb55ceaef
Andre <andrevieira19992018@gmail.com>
2025-11-20 20:37:53 -0300

    criando função exibirTextoNaTela

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-let titulo = document.querySelector("h1");
-let paragrafo = document.querySelector("p");
-
-titulo.innerHTML = "Jogo do número secreto";
-paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+function exibirTextoNaTela(tag,texto) {
+    let conteudo = document.querySelector(tag);
+    conteudo.innerHTML = texto; 
+}
 
  function verificarChute() {
-    
+    console.log("cliquei no botao")
  };
+
+
+
+exibirTextoNaTela("h1","Jogo do número secreto");
+exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+
+
