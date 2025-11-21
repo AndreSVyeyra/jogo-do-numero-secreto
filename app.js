292df6452663f6cfda4b89f96d4d5487c39bf4d9
Andre <andrevieira19992018@gmail.com>
2025-11-21 19:18:10 -0300

    alterando função verificarChute(), exibir quantidade de tentativa para acertos, com o operador ternario, caso uma tentativa palavra singular caso mas de uma palavra tentativa no plura.

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativa = 1;
+exibirTextoNaTela("h1","Jogo do número secreto");
+exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+
 
 function exibirTextoNaTela(tag,texto) {
     let conteudo = document.querySelector(tag);
@@ -7,25 +12,21 @@ function exibirTextoNaTela(tag,texto) {
  function verificarChute() {
     const chute = document.querySelector("input").value;
     if(chute == numeroSecreto){
+        let palavraTentativa = tentativa > 1 ? "tentativas" : "tentativa";
+        let mensagemTentativa = `Você descobriu o número secreto com ${tentativa} ${palavraTentativa}`;
         exibirTextoNaTela("h1", "Acertou!");
-        exibirTextoNaTela("p","Você descobriu o número secreto");
+        exibirTextoNaTela("p", mensagemTentativa);
     }else {
         if (chute > numeroSecreto){
             exibirTextoNaTela("p","O número secreto é menor");
         }else {
             exibirTextoNaTela("p","O número secreto é maior");
         }
+        tentativa++
     }
- };
-
-
-exibirTextoNaTela("h1","Jogo do número secreto");
-exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
-
+ }
 
 function gerarNumeroAleatorio(){
     const numeroAleatorio = parseInt(Math.random() * 10 + 1);
     return numeroAleatorio;
 }
-
-let numeroSecreto = gerarNumeroAleatorio();
